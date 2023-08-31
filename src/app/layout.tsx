6a3d90be5244b8bb98487259e77e3d823e614c9d
= <=>
2023-08-31 01:42:37 -0400

    Move globals.css to styles outside of app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { AppWrapper } from "'@/components/AppWrapper'";
-import './globals.css';
+import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
