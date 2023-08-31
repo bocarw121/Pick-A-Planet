4bf0cf8db3e473b8eba07688f9298e5dd08cf815
= <=>
2023-08-31 18:37:33 -0400

    Update prisma instance and schema

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+// Create an alias 'globalPrisma' which refers to the global object.
+const globalPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+// Export a constant 'prisma' which holds the PrismaClient instance.
+export const prisma =
+  globalPrisma.prisma ?? // Check if 'prisma' already exists in 'globalPrisma'.
+  new PrismaClient({
+    // If not, create a new PrismaClient instance.
+    log: ['query'], // configure PrismaClient to log queries.
+  });
+
+// In non-production environments, store the 'prisma' instance in 'globalPrisma'.
+if (process.env.NODE_ENV !== 'production') globalPrisma.prisma = prisma;
