2f2607f57e8e350e01975cca2b07acd29f007d07
= <=>
2023-09-02 16:50:22 -0400

    Add prisma adapter and add authOptions

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,12 +1,63 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { prisma } from "'@/lib/prisma'";
+import { verifyPassword } from "'@/lib/argon2'";
+
 export const nextAuthOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: <string>process.env.GOOGLE_CLIENT_ID,
+      clientSecret: <string>process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: <string>process.env.GITHUB_CLIENT_ID,
+      clientSecret: <string>process.env.GITHUB_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'email:',
+          type: 'email',
+          placeholder: 'your-awesome-username',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        // check to see if user exists
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        // check passwords match
+        const isMatch = verifyPassword(
+          user.hashedPassword,
+          credentials?.password,
+        );
+
+        if (!isMatch) {
+          return null;
+        }
+
+        return user;
+      },
     }),
   ],
+  adapter: PrismaAdapter(prisma),
   session: {
     // Choose how you want to save the user session.
     // The default is `"jwt"`, an encrypted JWT (JWE) stored in the session cookie.
@@ -14,7 +65,7 @@ export const nextAuthOptions: NextAuthOptions = {
     // You can still force a JWT session by explicitly defining `"jwt"`.
     // When using `"database"`, the session cookie will only contain a `sessionToken` value,
     // which is used to look up the session in the database.
-    strategy: 'database',
+    strategy: 'jwt',
 
     // Seconds - How long until an idle session expires and is no longer valid.
     maxAge: 30 * 24 * 60 * 60, // 30 days
@@ -24,4 +75,6 @@ export const nextAuthOptions: NextAuthOptions = {
     // Note: This option is ignored if using JSON Web Tokens
     updateAge: 24 * 60 * 60, // 24 hours
   },
+  secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === 'development',
 };
