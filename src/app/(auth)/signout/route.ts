a989e81b7815c89068c2ec8c2628b38addca8164
= <=>
2023-09-02 23:37:24 -0400

    Remove custom auth pages

--- src/app/(auth)/signout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export function GET(request: NextRequest) {
-  // const {} = getAuth(request);
-  console.log('hello from sign out handler');
-
-  return NextResponse.redirect(new URL('/', request.url));
-}
