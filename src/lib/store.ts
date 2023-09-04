921afb4f058ebee3eb394ed5996875c956ca39ca
= <=>
2023-09-04 14:40:14 -0400

    Add FormErrorStore

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,4 +1,4 @@
-import { User } from "'@/types'";
+import { FormError, User } from "'@/types'";
 import { create } from 'zustand';
 
 interface UserStore {
@@ -10,5 +10,39 @@ export const useUserStore = create<UserStore>((set) => ({
   user: null,
   setUser(user: User) {
     set(() => ({ user }));
+  },
+}));
+
+interface FormErrorStore {
+  error: FormError;
+  setError: (error: FormError) => void;
+  handleErrorChange: (type: string) => void;
+}
+
+export const useFormErrorStore = create<FormErrorStore>((set) => ({
+  error: {
+    message: '',
+    type: '',
+  },
+  setError: (error) =>
+    set((state) => ({
+      ...state,
+      error: {
+        message: error.message,
+        type: error.type,
+      },
+    })),
+  handleErrorChange(type: string) {
+    return set((state) => {
+      if (state.error.type !== type) return { ...state };
+
+      return {
+        ...state,
+        error: {
+          message: '',
+          type: '',
+        },
+      };
+    });
   },
 }));
