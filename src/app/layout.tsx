7d922294382e77121ca1595cf2dea927d17e3360
= <=>
2023-09-07 09:19:19 -0400

    Remove AppWrapper and make auth adjustments
    Simplified Context to export the SessionProvider and pass session as props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { getServerSession } from 'next-auth';
 
-import { AppWrapper } from "'@/components/AppWrapper'";
+import SessionProvider from "'@/context/SessionProvider'";
+import { Footer } from "'@/components/Footer'";
+import { NavBar } from "'@/components/NavBar'";
+import { ParticleHeader } from "'@/components/ParticleHeader'";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -16,10 +20,16 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen flex flex-col`}>
-        <AppWrapper>{children}</AppWrapper>
+        <SessionProvider session={session}>
+          <ParticleHeader />
+          <NavBar />
+          {children}
+          <Footer />
+        </SessionProvider>
       </body>
     </html>
   );
