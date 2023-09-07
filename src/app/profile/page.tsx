1b65606904ca4487bc1a6e3a699c39fb0b05dc59
= <=>
2023-09-07 10:48:31 -0400

    Update profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,5 +3,19 @@ import { getServerSession } from 'next-auth';
 export default async function Profile() {
   const session = await getServerSession();
 
-  return <h1>Welcome to the Profile page {session?.user?.name}</h1>;
+  return (
+    <div className="grid place-content-center text-center h-[80vh]">
+      <h1 className="text-2xl ">
+        Welcome to your Profile page {session?.user?.name}!{' '}
+      </h1>
+
+      <h4 className="text-[12rem] ">🚧</h4>
+
+      <p className="mt-8 text-xl">
+        This page is under construction. Please check back later for more
+        updates. In the meantime, You can learn about the planets in our solar
+        with a personalized experience.
+      </p>
+    </div>
+  );
 }
